a9408070a341b847aaffe96ec967cf6ec34172cd
ashimlamsal93 <ashimlamsal93@gmail.com>
2025-12-10 22:13:56 +0545

    Initial commit with database

--- src/group1_uunchai/Group1_Uunchai.java
+++ src/group1_uunchai/Group1_Uunchai.java
@@ -1,20 +1,52 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package group1_uunchai;
+import database.Database;
+
+import database.MySqlConnection;
+
+
 
 /**
+
  *
- * @author Acer
+
+ * @author User
+
  */
+
 public class Group1_Uunchai {
 
+
+
     /**
+
      * @param args the command line arguments
+
      */
+
     public static void main(String[] args) {
+
         // TODO code application logic here
+
+       Database db = new MySqlConnection();
+
+       if(db.openConnection() !=null){
+
+           System.out.println("Connectioon succesful");
+
+         
+
+       }else{
+
+           System.out.println("Not successful");
+
+       }
+
     }
+
     
+
 }
