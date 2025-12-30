f9e51ab5b4d40f17d4a2662f0d13aea22b2169d0
sunilkdka <suneelkhadka2060@gmail.com>
2025-12-30 10:07:52 +0545

    Refactor main method to use RegistrationController

--- src/group1_uunchai/Group1_Uunchai.java
+++ src/group1_uunchai/Group1_Uunchai.java
@@ -4,14 +4,17 @@
  */
 package group1_uunchai;
 
-import view.AdminLogin;
-import view.Userprofile;
+// import view.AdminLogin;
+import controller.RegistrationController;
+// import view.Userprofile;
+import view.Registration;
 
 
 
 public class Group1_Uunchai {
     public static void main(String []args) {
-        // new AdminLogin().setVisible(true);
-        new Userprofile().setVisible(true);
-    }
-}
+            Registration reg = new Registration();
+        RegistrationController userC = new RegistrationController(reg);
+        userC.open();
+    }}
+      
